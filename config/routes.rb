7a7448fb489e714764866b32276d0e5f361b02d8
sato-tkm <t.28pnorudo@gmail.com>
2021-01-05 12:19:15 +0000

    update

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get 'home/about' => 'homes#about'
+  root to: 'homes#top'
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  end
