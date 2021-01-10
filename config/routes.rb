9727257d28e0228e637d31e24c36e0b569d446b5
sato-tkm <t.28pnorudo@gmail.com>
2021-01-10 12:59:39 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   root to: 'homes#top'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :relationships, only: [:create, :destroy]
+  resources :users,only: [:show,:index,:edit,:update] do
+    get "followers"=>"relationships#followers"
+    get "follows"=>"relationships#follows"
+  end
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_114704) do
+ActiveRecord::Schema.define(version: 2021_01_09_082430) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_01_07_114704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "book_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
