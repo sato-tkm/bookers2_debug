7b26b4a65c29335637ad60fdb67181b0ede874db
sato-tkm <t.28pnorudo@gmail.com>
2021-01-13 11:02:48 +0000

    Update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,4 +2,17 @@ class SearchController < ApplicationController
   def index
     @users = User.where(name: params[:search])
   end
+
+  def search
+  　　 @range = params[:range]
+  　　 search = params[:search]
+  　　 word = params[:word]
+
+  if @range == '1'
+          @user = User.search(search,word)
+  else
+    　　　 @book = Book.search(search,word)
+　 end
+　 end
+
 end
